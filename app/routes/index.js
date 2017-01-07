702ed0388c1b7f7fd6438339e3c63278b5917b6b
Jesse Walling <jessephilipwalling@gmail.com>
2017-01-07 10:10:10 -0800

    create get and post routes to index.js for text posts

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var textPost = require('../dbModels/textPost.js');
+var bodyParser = require('body-parser');
 
-/* GET home page. */
+/* GET home page and specify variables. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    textPost.count({}, function(err, documentCount){
+	textPost.find({}, function(err, postData) {
+	    if (err) return next(err);
+	    res.render('index', { title: 'Express', post: postData, count: documentCount });
+	});
+    });
 });
+
+/* POST request to /newText creates new text post */
+router.post('/newText', function(req, res, next) {
+    var title = req.body.title;
+    var content = req.body.content;
+    var tags = req.body.tags;
+
+    textPost.create({title: title, content: content, tags: tags}, function(err, textPost) {
+	if (err) return next(err);
+	res.json(textPost);
+    });
+});
+
+// INSERT IMAGE/FILE UPLOAD ENPOINT HERE
 
 module.exports = router;
