0d41a8b25585a93e4b7c891a7e38c543904116f0
Jesse Walling <jessephilipwalling@gmail.com>
2017-01-11 20:01:15 -0800

    add routes for initial tag filtering, parse form submit by commas for tags and url links

--- app/routes/index.js
+++ app/routes/index.js
@@ -35,10 +35,12 @@ router.post('/newText', function(req, res, next) {
     var title = req.body.title;
     var content = req.body.content;
     var tags = req.body.tags;
-
-    Posts.create({type: type, title: title, content: content, tags: tags}, function(err, textPost) {
+    var tagsSplit = tags.split(', ');
+    
+    Posts.create({type: type, title: title, content: content, tags: tagsSplit}, function(err, textPost) {
 	if (err) return next(err);
 	res.send('<p>Permanent post URL: example.com/postId/' + textPost._id + '</p><br><a href="/">Back to home</a>');
+	console.log(textPost.tags);
     });
 });
 
@@ -46,12 +48,22 @@ router.post('/newImage', function(req, res, next) {
     var type = req.body.type;
     var title = req.body.title;
     var content = req.body.content;
+    var contentSplit = content.split(', ');
     var caption = req.body.caption;
     var tags = req.body.tags;
+    var tagsSplit = tags.split(', ');
 
-    Posts.create({type: type, title: title, content: content, tags: tags}, function(err, imagePost) {
+    Posts.create({type: type, title: title, content: contentSplit, caption: caption, tags: tagsSplit}, function(err, imagePost) {
 	if (err) return next(err);
 	res.send('<p>Permanent post URL: example.com/postId/' + imagePost._id + '</p><br><a href="/">Back to home</a>');
+    });
+});
+
+router.get('/tag/:tag', function (req, res, next) {
+    Posts.count({tags: req.params.tag}, function(err, postCount) {
+	Posts.find({tags: req.params.tag}, function(err, postData) {
+	    res.render('tag/index', {post: postData, count: postCount});
+	});
     });
 });
 
